656076152503ddb66ada9bdac768e4ad09a1b2e0
Leon <dev@myzelyam.de>
2018-04-07 12:52:19 +0200

    Register spring thread scope

--- src/main/java/rocks/cleanstone/Cleanstone.java
+++ src/main/java/rocks/cleanstone/Cleanstone.java
@@ -1,13 +1,17 @@
 package rocks.cleanstone;
 
+import org.springframework.beans.factory.config.Scope;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.SimpleThreadScope;
 
 public class Cleanstone {
 
     public static void main(String[] args) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("rocks/cleanstone/CleanstoneServer.xml");
         context.registerShutdownHook();
+        Scope threadScope = new SimpleThreadScope();
+        context.getBeanFactory().registerScope("thread", threadScope);
 
 
         CleanstoneServer cleanstoneServer = (CleanstoneServer) context.getBean("cleanstoneServer");
