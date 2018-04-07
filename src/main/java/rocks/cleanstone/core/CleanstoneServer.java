5fb62ab0fe452e738508fa98ec44e0eed4003fe7
Tim Windelschmidt <fionera@fionera.de>
2018-04-07 16:46:13 +0200

    Add ApplicationEventPublisher

--- src/main/java/rocks/cleanstone/core/CleanstoneServer.java
+++ src/main/java/rocks/cleanstone/core/CleanstoneServer.java
@@ -1,11 +1,15 @@
 package rocks.cleanstone.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import rocks.cleanstone.net.Networking;
 
 public abstract class CleanstoneServer {
     @Autowired
     protected Networking networking;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     public abstract void run();
 }
