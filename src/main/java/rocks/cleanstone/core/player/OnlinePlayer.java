ce895d5374a962ed7b03e907484a20c5671c4174
Leon <dev@myzelyam.de>
2018-04-07 19:03:13 +0200

    Expand OnlinePlayer

--- src/main/java/rocks/cleanstone/core/player/OnlinePlayer.java
+++ src/main/java/rocks/cleanstone/core/player/OnlinePlayer.java
@@ -2,9 +2,21 @@ package rocks.cleanstone.core.player;
 
 import rocks.cleanstone.net.PlayerConnection;
 
-public class OnlinePlayer extends User {
+public class OnlinePlayer {
 
-    public OnlinePlayer(PlayerId playerId, PlayerConnection connection) {
-        super(playerId);
+    private final PlayerId id;
+    private final PlayerConnection connection;
+
+    public OnlinePlayer(PlayerId id, PlayerConnection connection) {
+        this.id = id;
+        this.connection = connection;
+    }
+
+    public PlayerId getId() {
+        return id;
+    }
+
+    public PlayerConnection getConnection() {
+        return connection;
     }
 }
--- src/main/java/rocks/cleanstone/core/player/User.java
+++
@@ -1,7 +0,0 @@
-package rocks.cleanstone.core.player;
-
-public class User {
-    public User(PlayerId playerId) {
-
-    }
-}
