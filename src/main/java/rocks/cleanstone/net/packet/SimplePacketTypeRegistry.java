77987c4bf4da7baa56e38e3ad46912a322040fb2
Leon <dev@myzelyam.de>
2018-04-15 00:00:47 +0200

    Move packet type registration to protocol

--- src/main/java/rocks/cleanstone/net/packet/SimplePacketTypeRegistry.java
+++ src/main/java/rocks/cleanstone/net/packet/SimplePacketTypeRegistry.java
@@ -6,20 +6,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Set;
 
-import rocks.cleanstone.net.packet.cleanstone.CleanstoneReceivePacketType;
-import rocks.cleanstone.net.packet.cleanstone.CleanstoneSendPacketType;
-import rocks.cleanstone.net.packet.minecraft.MinecraftReceivePacketType;
-import rocks.cleanstone.net.packet.minecraft.MinecraftSendPacketType;
-
 public class SimplePacketTypeRegistry implements PacketTypeRegistry {
 
     private Set<PacketType> packetTypes = Sets.newConcurrentHashSet();
 
     public void init() {
-        registerPacketType(CleanstoneReceivePacketType.values());
-        registerPacketType(CleanstoneSendPacketType.values());
-        registerPacketType(MinecraftReceivePacketType.values());
-        registerPacketType(MinecraftSendPacketType.values());
     }
 
     @Override
--- src/main/java/rocks/cleanstone/net/packet/protocol/minecraft/AbstractMinecraftProtocol.java
+++ src/main/java/rocks/cleanstone/net/packet/protocol/minecraft/AbstractMinecraftProtocol.java
@@ -1,11 +1,23 @@
 package rocks.cleanstone.net.packet.protocol.minecraft;
 
 import rocks.cleanstone.net.packet.Packet;
-import rocks.cleanstone.net.packet.protocol.PacketCodec;
 import rocks.cleanstone.net.packet.PacketTypeRegistry;
+import rocks.cleanstone.net.packet.SimplePacketTypeRegistry;
+import rocks.cleanstone.net.packet.minecraft.MinecraftReceivePacketType;
+import rocks.cleanstone.net.packet.minecraft.MinecraftSendPacketType;
+import rocks.cleanstone.net.packet.protocol.PacketCodec;
 import rocks.cleanstone.net.packet.protocol.Protocol;
 
 public class AbstractMinecraftProtocol implements Protocol {
+
+    private PacketTypeRegistry registry;
+
+    public void init() {
+        registry = new SimplePacketTypeRegistry();
+        registry.registerPacketType(MinecraftSendPacketType.values());
+        registry.registerPacketType(MinecraftReceivePacketType.values());
+    }
+
     @Override
     public <T extends Packet> PacketCodec<T> getPacketCodec(Class<T> packet) {
         return null;
@@ -13,6 +25,6 @@ public class AbstractMinecraftProtocol implements Protocol {
 
     @Override
     public PacketTypeRegistry getPacketTypeRegistry() {
-        return null;
+        return registry;
     }
 }
