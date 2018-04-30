af95b2d4400b0151d76ec433f0e735c66be1ab0c
Leon Mangler <dev@myzelyam.de>
2018-04-30 14:28:34 +0200

    Send SetCompression before LoginSuccess in LoginMgr

--- src/main/java/rocks/cleanstone/net/minecraft/login/LoginManager.java
+++ src/main/java/rocks/cleanstone/net/minecraft/login/LoginManager.java
@@ -13,6 +13,7 @@ import rocks.cleanstone.net.minecraft.packet.data.Chat;
 import rocks.cleanstone.net.minecraft.packet.inbound.EncryptionResponsePacket;
 import rocks.cleanstone.net.minecraft.packet.outbound.DisconnectLoginPacket;
 import rocks.cleanstone.net.minecraft.packet.outbound.LoginSuccessPacket;
+import rocks.cleanstone.net.minecraft.packet.outbound.SetCompressionPacket;
 import rocks.cleanstone.net.minecraft.protocol.VanillaProtocolState;
 import rocks.cleanstone.net.utils.SecurityUtils;
 import rocks.cleanstone.net.utils.UUIDUtils;
@@ -47,8 +48,9 @@ public class LoginManager {
                             SessionServerResponse.Property textures) {
         if (connectionLoginDataMap.remove(connection) == null)
             throw new IllegalStateException("Cannot finish login before it has started");
-
+        SetCompressionPacket setCompressionPacket = new SetCompressionPacket(Short.MAX_VALUE);
         LoginSuccessPacket loginSuccessPacket = new LoginSuccessPacket(uuid, accountName);
+        connection.sendPacket(setCompressionPacket);
         connection.sendPacket(loginSuccessPacket);
         connection.setProtocolState(VanillaProtocolState.PLAY);
         // TODO Initialize and handle OnlinePlayer
