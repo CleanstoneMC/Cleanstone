77e50129ff809b56c1a624005f4a82b347b5e5e3
Leon Mangler <dev@myzelyam.de>
2018-05-07 17:12:29 +0200

    Merge SimpleWorldManager and SimpleOpenWorldGame

--- src/main/java/rocks/cleanstone/game/OpenWorldGame.java
+++ src/main/java/rocks/cleanstone/game/OpenWorldGame.java
@@ -3,8 +3,7 @@ package rocks.cleanstone.game;
 import rocks.cleanstone.core.CleanstoneMainServer;
 import rocks.cleanstone.game.world.WorldManager;
 
-public interface OpenWorldGame {
-    WorldManager getWorldManager();
+public interface OpenWorldGame extends WorldManager {
 
     CleanstoneMainServer getServer();
 }
--- src/main/java/rocks/cleanstone/game/SimpleOpenWorldGame.java
+++ src/main/java/rocks/cleanstone/game/SimpleOpenWorldGame.java
@@ -1,22 +1,61 @@
 package rocks.cleanstone.game;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.concurrent.ListenableFuture;
 import rocks.cleanstone.core.CleanstoneMainServer;
-import rocks.cleanstone.game.world.SimpleWorldManager;
-import rocks.cleanstone.game.world.WorldManager;
+import rocks.cleanstone.game.world.World;
+import rocks.cleanstone.game.world.WorldGenerator;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
 
 public class SimpleOpenWorldGame implements OpenWorldGame {
 
-    private final WorldManager worldManager;
     private final CleanstoneMainServer server;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public SimpleOpenWorldGame(CleanstoneMainServer server) {
-        worldManager = new SimpleWorldManager(this);
         this.server = server;
+        for (String autoLoadWorldID : server.getMinecraftConfig().getAutoLoadWorlds()) {
+            loadWorld(autoLoadWorldID);
+        }
+    }
+
+    private final Collection<World> loadedWorlds = Sets.newConcurrentHashSet();
+
+    @Override
+    public Collection<World> getLoadedWorlds() {
+        return ImmutableSet.copyOf(loadedWorlds);
+    }
+
+    @Nullable
+    @Override
+    public World getLoadedWorld(String id) {
+        return loadedWorlds.stream().filter(w -> w.getID().equals(id)).findFirst().orElse(null);
     }
 
     @Override
-    public WorldManager getWorldManager() {
-        return worldManager;
+    public ListenableFuture<World> loadWorld(String id) {
+        logger.info("Loading world '" + id + "'");
+        return null;
+    }
+
+    @Override
+    public void unloadWorld(String id) {
+
+    }
+
+    @Override
+    public void createWorld(String id, WorldGenerator generator) {
+
+    }
+
+    @Override
+    public void deleteWorld(String id) {
+
     }
 
     public CleanstoneMainServer getServer() {
