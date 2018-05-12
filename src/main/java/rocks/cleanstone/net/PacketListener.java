d57450351131857485f4de8217e2d76f079b5331
Leon Mangler <dev@myzelyam.de>
2018-05-12 11:52:22 +0200

    Remove unused PacketListener classes

--- src/main/java/rocks/cleanstone/net/PacketListener.java
+++
@@ -1,9 +0,0 @@
-package rocks.cleanstone.net;
-
-import rocks.cleanstone.net.packet.Packet;
-
-public interface PacketListener {
-    void onReceive(Packet packet, Connection connection);
-
-    void onSend(Packet packet, Connection connection);
-}
