1a920fc599c8739be54b40b6cd8b6165856046a9
Tim Windelschmidt <fionera@fionera.de>
2018-05-14 18:42:36 +0000

    Add Chatmessage Codec. Somehow it does not work...

--- src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_12_2/MinecraftProtocolLayer_v1_12_2.java
+++ src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_12_2/MinecraftProtocolLayer_v1_12_2.java
@@ -1,34 +1,11 @@
 package rocks.cleanstone.net.minecraft.protocol.v1_12_2;
 
-import rocks.cleanstone.net.minecraft.packet.inbound.ClientSettingsPacket;
-import rocks.cleanstone.net.minecraft.packet.inbound.EncryptionResponsePacket;
-import rocks.cleanstone.net.minecraft.packet.inbound.HandshakePacket;
-import rocks.cleanstone.net.minecraft.packet.inbound.InKeepAlivePacket;
-import rocks.cleanstone.net.minecraft.packet.inbound.LoginStartPacket;
-import rocks.cleanstone.net.minecraft.packet.inbound.PingPacket;
-import rocks.cleanstone.net.minecraft.packet.inbound.RequestPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.ChunkDataPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.DisconnectLoginPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.DisconnectPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.EncryptionRequestPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.JoinGamePacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.LoginSuccessPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.OutKeepAlivePacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.OutPlayerAbilitiesPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.OutPlayerPositionAndLookPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.PongPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.ResponsePacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.SetCompressionPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.SpawnPositionPacket;
+import rocks.cleanstone.net.minecraft.packet.inbound.*;
+import rocks.cleanstone.net.minecraft.packet.inbound.ChatMessagePacket;
+import rocks.cleanstone.net.minecraft.packet.outbound.*;
 import rocks.cleanstone.net.minecraft.protocol.MinecraftClientProtocolLayer;
 import rocks.cleanstone.net.minecraft.protocol.MinecraftServerProtocolLayer;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.ClientSettingsCodec;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.EncryptionResponseCodec;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.HandshakeCodec;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.InKeepAliveCodec;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.LoginStartCodec;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.PingCodec;
-import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.RequestCodec;
+import rocks.cleanstone.net.minecraft.protocol.v1_12_2.inbound.*;
 import rocks.cleanstone.net.minecraft.protocol.v1_12_2.outbound.ChunkDataCodec;
 import rocks.cleanstone.net.minecraft.protocol.v1_12_2.outbound.DisconnectCodec;
 import rocks.cleanstone.net.minecraft.protocol.v1_12_2.outbound.DisconnectLoginCodec;
@@ -54,6 +31,7 @@ public class MinecraftProtocolLayer_v1_12_2 extends MinecraftServerProtocolLayer
         registerPacketCodec(new PingCodec(), PingPacket.class);
         registerPacketCodec(new ClientSettingsCodec(), ClientSettingsPacket.class);
         registerPacketCodec(new InKeepAliveCodec(), InKeepAlivePacket.class);
+        registerPacketCodec(new ChatMessageCodec(), ChatMessagePacket.class);
 
         // outbound
         registerPacketCodec(new DisconnectCodec(), DisconnectPacket.class);
