ded919429303729b82e7e615ba5e3af5123b0af6
Leon Mangler <dev@myzelyam.de>
2018-05-22 14:12:46 +0200

    Always send ChunkData on init

--- src/main/java/rocks/cleanstone/player/initialize/WorldPackets.java
+++ src/main/java/rocks/cleanstone/player/initialize/WorldPackets.java
@@ -2,31 +2,20 @@ package rocks.cleanstone.player.initialize;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
-
-import java.util.Collections;
-import java.util.concurrent.ThreadLocalRandom;
-
 import rocks.cleanstone.game.Position;
-import rocks.cleanstone.game.block.ImmutableBlock;
 import rocks.cleanstone.game.entity.Rotation;
 import rocks.cleanstone.game.entity.vanilla.SimpleHuman;
-import rocks.cleanstone.game.material.VanillaMaterial;
 import rocks.cleanstone.game.world.generation.FlatWorldGenerator;
-import rocks.cleanstone.game.world.region.chunk.ArrayChunkTable;
-import rocks.cleanstone.game.world.region.chunk.ChunkTable;
-import rocks.cleanstone.game.world.region.chunk.SimpleChunk;
 import rocks.cleanstone.game.world.region.chunk.vanilla.ChunkDataPacketFactory;
 import rocks.cleanstone.net.minecraft.packet.enums.Difficulty;
 import rocks.cleanstone.net.minecraft.packet.enums.Dimension;
 import rocks.cleanstone.net.minecraft.packet.enums.LevelType;
 import rocks.cleanstone.net.minecraft.packet.enums.PlayerAbilities;
-import rocks.cleanstone.net.minecraft.packet.outbound.ChunkDataPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.JoinGamePacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.OutPlayerAbilitiesPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.OutPlayerPositionAndLookPacket;
-import rocks.cleanstone.net.minecraft.packet.outbound.SpawnPositionPacket;
+import rocks.cleanstone.net.minecraft.packet.outbound.*;
 import rocks.cleanstone.player.Player;
 import rocks.cleanstone.player.event.AsyncPlayerInitializationEvent;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class WorldPackets {
 
@@ -45,14 +34,14 @@ public class WorldPackets {
         player.sendPacket(new OutPlayerPositionAndLookPacket(position.getX(), position.getY(), position.getZ(), rotation.getYaw(), rotation.getPitch(), 0, ThreadLocalRandom.current().nextInt()));
         //player.sendPacket(new WindowItemsPacket(0,));
 
-//        FlatWorldGenerator flatWorldGenerator = new FlatWorldGenerator();
-//
-//        for (int x = 0; x < 14; x++) {
-//            for (int z = 0; z < 14; z++) {
-//                ChunkDataPacket chunkDataPacket = ChunkDataPacketFactory.create(
-//                        flatWorldGenerator.generateChunk(x - 7, z - 7), true);
-//                player.sendPacket(chunkDataPacket);
-//            }
-//        }
+        FlatWorldGenerator flatWorldGenerator = new FlatWorldGenerator();
+
+        for (int x = 0; x < 14; x++) {
+            for (int z = 0; z < 14; z++) {
+                ChunkDataPacket chunkDataPacket = ChunkDataPacketFactory.create(
+                        flatWorldGenerator.generateChunk(x - 7, z - 7), true);
+                player.sendPacket(chunkDataPacket);
+            }
+        }
     }
 }
