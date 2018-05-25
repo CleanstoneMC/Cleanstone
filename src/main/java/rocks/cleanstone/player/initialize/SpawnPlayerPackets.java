d3db5cabc77c0d3a284613e06010c740ab2032ff
Tim Windelschmidt <fionera@fionera.de>
2018-05-25 19:39:22 +0000

    Move SpawnPlayerPackets to own Listener

---
+++ src/main/java/rocks/cleanstone/player/initialize/SpawnPlayerPackets.java
@@ -0,0 +1,55 @@
+package rocks.cleanstone.player.initialize;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
+import rocks.cleanstone.net.minecraft.packet.outbound.SpawnPlayerPacket;
+import rocks.cleanstone.player.Player;
+import rocks.cleanstone.player.PlayerManager;
+import rocks.cleanstone.player.event.AsyncPlayerInitializationEvent;
+
+public class SpawnPlayerPackets {
+
+    private final PlayerManager playerManager;
+
+    public SpawnPlayerPackets(PlayerManager playerManager) {
+        this.playerManager = playerManager;
+    }
+
+    @Order(value = 31)
+    @EventListener
+    @Async
+    public void onJoin(AsyncPlayerInitializationEvent e) {
+        Player player = e.getPlayer();
+
+        sendToJoined(player);
+        sendToOther(player);
+    }
+
+    /**
+     * This sends the Spawn Packet for the Spawned User to the other Users
+     */
+    private void sendToOther(Player player) {
+        SpawnPlayerPacket spawnPlayerPacket = new SpawnPlayerPacket(player.getEntity().getEntityID(), player.getId().getUUID(), player.getEntity().getPosition().getX(), player.getEntity().getPosition().getY(), player.getEntity().getPosition().getZ(),
+                player.getEntity().getRotation().getYaw(), player.getEntity().getRotation().getPitch(), null); //TODO: Add Metadata
+
+        playerManager.broadcastPacket(spawnPlayerPacket, player);
+    }
+
+    /**
+     *  This sends the Player Spawn Packet for every Player to the Joined User
+     */
+    private void sendToJoined(Player player) {
+        playerManager.getOnlinePlayers().forEach(
+                onlinePlayer -> {
+                    if (onlinePlayer == player) {
+                        return;
+                    }
+
+                    SpawnPlayerPacket spawnPlayerPacket = new SpawnPlayerPacket(onlinePlayer.getEntity().getEntityID(), onlinePlayer.getId().getUUID(), onlinePlayer.getEntity().getPosition().getX(), onlinePlayer.getEntity().getPosition().getY(), onlinePlayer.getEntity().getPosition().getZ(),
+                            onlinePlayer.getEntity().getRotation().getYaw(), onlinePlayer.getEntity().getRotation().getPitch(), null); //TODO: Add Metadata
+
+                    player.sendPacket(spawnPlayerPacket);
+                });
+    }
+}
--- src/main/java/rocks/cleanstone/player/initialize/TablistPackets.java
+++ src/main/java/rocks/cleanstone/player/initialize/TablistPackets.java
@@ -32,20 +32,6 @@ public class TablistPackets {
                 onlinePlayer -> packet.getPlayers().add(new PlayerListItemPacket.PlayerItem(onlinePlayer)));
 
         player.sendPacket(packet);
-
-
-        // This sends the Player Spawn Packet for every Player to the Joined User
-        playerManager.getOnlinePlayers().forEach(
-                onlinePlayer -> {
-                    if (onlinePlayer == player) {
-                        return;
-                    }
-
-                    SpawnPlayerPacket spawnPlayerPacket = new SpawnPlayerPacket(onlinePlayer.getEntity().getEntityID(), onlinePlayer.getId().getUUID(), onlinePlayer.getEntity().getPosition().getX(), onlinePlayer.getEntity().getPosition().getY(), onlinePlayer.getEntity().getPosition().getZ(),
-                            onlinePlayer.getEntity().getRotation().getYaw(), onlinePlayer.getEntity().getRotation().getPitch(), null); //TODO: Add Metadata
-
-                    player.sendPacket(spawnPlayerPacket);
-                });
     }
 
     public void broadcastAddition(Player player, Player... broadcastExemptions) {
@@ -53,12 +39,6 @@ public class TablistPackets {
         packet.getPlayers().add(new PlayerListItemPacket.PlayerItem(player));
 
         playerManager.broadcastPacket(packet, broadcastExemptions);
-
-        // This sends the Spawn Packet for the Spawned User to the other Users
-        SpawnPlayerPacket spawnPlayerPacket = new SpawnPlayerPacket(player.getEntity().getEntityID(), player.getId().getUUID(), player.getEntity().getPosition().getX(), player.getEntity().getPosition().getY(), player.getEntity().getPosition().getZ(),
-                player.getEntity().getRotation().getYaw(), player.getEntity().getRotation().getPitch(), null); //TODO: Add Metadata
-
-        playerManager.broadcastPacket(spawnPlayerPacket, player);
     }
 
 }
