7bc60fb3691a94e727dca71356ed5f479f3d64cf
Leon Mangler <dev@myzelyam.de>
2018-05-26 16:32:07 +0200

    Refactor and expand Rotation

--- src/main/java/rocks/cleanstone/game/entity/Rotation.java
+++ src/main/java/rocks/cleanstone/game/entity/Rotation.java
@@ -1,25 +1,19 @@
 package rocks.cleanstone.game.entity;
 
-public class Rotation {
-    private float pitch;
-    private float yaw;
+import rocks.cleanstone.utils.Vector;
 
-    public Rotation(float pitch, float yaw) {
-        this.pitch = pitch;
+public class Rotation {
+    private float yaw;
+    private float pitch;
+
+    public Rotation(float yaw, float pitch) {
         this.yaw = yaw;
+        this.pitch = pitch;
     }
 
     public Rotation(Rotation rotation) {
-        this.pitch = rotation.getPitch();
         this.yaw = rotation.getYaw();
-    }
-
-    public float getPitch() {
-        return pitch;
-    }
-
-    public void setPitch(float pitch) {
-        this.pitch = pitch;
+        this.pitch = rotation.getPitch();
     }
 
     public float getYaw() {
@@ -30,6 +24,14 @@ public class Rotation {
         this.yaw = yaw;
     }
 
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(float pitch) {
+        this.pitch = pitch;
+    }
+
     public int getIntYaw() {
         return (int) (yaw % 360 / 360 * 256);
     }
@@ -38,4 +40,10 @@ public class Rotation {
         return (int) (pitch % 360 / 360 * 256);
     }
 
+    public Vector toUnitVector() {
+        double x = -Math.cos(pitch) * Math.sin(yaw);
+        double y = -Math.sin(pitch);
+        double z = Math.cos(pitch) * Math.cos(yaw);
+        return new Vector(x, y, z);
+    }
 }
