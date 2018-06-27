91efc53d70d2dde783b3ed10ffd08ffe3fd34901
succcubbus <jannis.kaiser2@gmail.com>
2018-06-28 01:29:46 +0200

    remove previous completion files

--- src/main/java/rocks/cleanstone/game/command/tabcompleter/TabCompleter.java
+++
@@ -1,9 +0,0 @@
-package rocks.cleanstone.game.command.tabcompleter;
-
-public interface TabCompleter {
-    Class getCompletingClass();
-
-    String[] getCompletion();
-
-    String[] getCompletion(String message);
-}
