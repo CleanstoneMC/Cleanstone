d20c39e684fa3d6d936d1693a932682cd3423648
Leon Mangler <dev@myzelyam.de>
2018-06-30 17:45:12 +0200

    Use ByteBufIn/OutputStreams in JavaSerializableCodec

--- src/main/java/rocks/cleanstone/data/JavaSerializableCodec.java
+++ src/main/java/rocks/cleanstone/data/JavaSerializableCodec.java
@@ -1,21 +1,19 @@
 package rocks.cleanstone.data;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.Unpooled;
 
 public class JavaSerializableCodec<T extends Serializable> implements Codec<T, ByteBuf> {
     @Override
     public T deserialize(ByteBuf data) throws IOException {
-        byte[] bytes = new byte[data.readableBytes()];
-        data.readBytes(bytes);
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new ByteBufInputStream(data))) {
             //noinspection unchecked
             return (T) objectInputStream.readObject();
         } catch (ClassNotFoundException e) {
@@ -25,10 +23,11 @@ public class JavaSerializableCodec<T extends Serializable> implements Codec<T, B
 
     @Override
     public ByteBuf serialize(T object) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+        ByteBuf data = Unpooled.buffer();
+        ByteBufOutputStream byteBufOutputStream = new ByteBufOutputStream(data);
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteBufOutputStream)) {
             objectOutputStream.writeObject(object);
         }
-        return Unpooled.wrappedBuffer(byteArrayOutputStream.toByteArray());
+        return data;
     }
 }
