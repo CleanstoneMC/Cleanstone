72008b42042b87ac5c240c235d1d0461ef92c86c
Leon Mangler <dev@myzelyam.de>
2018-08-10 15:58:43 +0200

    Only allow changing protocol state during handshake

--- src/main/java/rocks/cleanstone/net/minecraft/HandshakeListener.java
+++ src/main/java/rocks/cleanstone/net/minecraft/HandshakeListener.java
@@ -5,9 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import rocks.cleanstone.net.event.InboundPacketEvent;
-import rocks.cleanstone.net.packet.inbound.HandshakePacket;
 import rocks.cleanstone.net.minecraft.protocol.MinecraftClientProtocolLayer;
 import rocks.cleanstone.net.minecraft.protocol.VanillaProtocolState;
+import rocks.cleanstone.net.packet.inbound.HandshakePacket;
 import rocks.cleanstone.net.protocol.ClientProtocolLayer;
 
 public class HandshakeListener {
@@ -24,8 +24,8 @@ public class HandshakeListener {
             if (updatedLayer != null) event.getConnection().setClientProtocolLayer(updatedLayer);
 
             VanillaProtocolState updatedState = VanillaProtocolState.byStateID(packet.getState());
-            if (updatedState != null && (updatedState == VanillaProtocolState.STATUS
-                    || updatedState == VanillaProtocolState.LOGIN)) {
+            if ((updatedState == VanillaProtocolState.STATUS || updatedState == VanillaProtocolState.LOGIN)
+                    && event.getConnection().getProtocolState() == VanillaProtocolState.HANDSHAKE) {
                 event.getConnection().setProtocolState(updatedState);
             }
         }
