03468190d0b6b69f159a0c2636847a8d2d45370d
Leon Mangler <dev@myzelyam.de>
2018-08-18 17:35:05 +0200

    Sanitize property de/serialization input

--- src/main/java/rocks/cleanstone/game/block/state/property/type/PropertyInteger.java
+++ src/main/java/rocks/cleanstone/game/block/state/property/type/PropertyInteger.java
@@ -1,28 +1,32 @@
 package rocks.cleanstone.game.block.state.property.type;
 
+import com.google.common.base.Preconditions;
 import com.google.common.math.IntMath;
 
 import java.math.RoundingMode;
 
 public class PropertyInteger extends AbstractProperty<Integer> {
 
-    private final int totalValuesAmount, minValue, maxSerializationBits;
+    private final int totalValuesAmount, minValue, maxValue, maxSerializationBits;
 
     public PropertyInteger(String key, int minValue, int maxValue, Integer defaultValue) {
         super(key, defaultValue);
         this.minValue = minValue;
+        this.maxValue = maxValue;
         totalValuesAmount = maxValue - minValue + 1;
         this.maxSerializationBits = IntMath.log2(totalValuesAmount, RoundingMode.CEILING);
     }
 
     @Override
     public int serialize(Integer value) {
+        Preconditions.checkPositionIndex(minValue, maxValue);
         return value - minValue;
     }
 
     @Override
     public Integer deserialize(int serializedValue) {
-        return serializedValue;
+        Preconditions.checkPositionIndex(0, totalValuesAmount - 1);
+        return serializedValue + minValue;
     }
 
     @Override
