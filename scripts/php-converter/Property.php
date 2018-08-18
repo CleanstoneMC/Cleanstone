aebc8a916d1cd4ba034da193e1a8e8752e46cf27
Tim Windelschmidt <fionera@fionera.de>
2018-08-18 19:28:58 +0200

    Add Mapping for some Enums and add generated VanillaBlockType Enum

--- scripts/php-converter/Property.php
+++ scripts/php-converter/Property.php
@@ -3,6 +3,103 @@
 
 class Property
 {
+    const BLOCK_NAME_ENUM_OVERRIDE = [
+        'powered_rail' => ['shape' => 'RailShape'],
+        'detector_rail' => ['shape' => 'RailShape'],
+        'rail' => ['shape' => 'RailShape'],
+        'activator_rail' => ['shape' => 'RailShape'],
+
+        'moving_piston' => ['type' => 'PistonType'],
+        'piston_head' => ['type' => 'PistonType'],
+
+        'white_bed' => ['part' => 'BedPart'],
+        'orange_bed' => ['part' => 'BedPart'],
+        'magenta_bed' => ['part' => 'BedPart'],
+        'light_blue_bed' => ['part' => 'BedPart'],
+        'yellow_bed' => ['part' => 'BedPart'],
+        'lime_bed' => ['part' => 'BedPart'],
+        'pink_bed' => ['part' => 'BedPart'],
+        'gray_bed' => ['part' => 'BedPart'],
+        'light_gray_bed' => ['part' => 'BedPart'],
+        'cyan_bed' => ['part' => 'BedPart'],
+        'purple_bed' => ['part' => 'BedPart'],
+        'blue_bed' => ['part' => 'BedPart'],
+        'brown_bed' => ['part' => 'BedPart'],
+        'green_bed' => ['part' => 'BedPart'],
+        'red_bed' => ['part' => 'BedPart'],
+        'black_bed' => ['part' => 'BedPart'],
+
+        'chest' => ['type' => 'ChestType'],
+        'trapped_chest' => ['type' => 'ChestType'],
+
+        'oak_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+        'iron_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+
+        'oak_trapdoor' => ['half' => 'HalfBlockPosition'],
+        'spruce_trapdoor' => ['half' => 'HalfBlockPosition'],
+        'birch_trapdoor' => ['half' => 'HalfBlockPosition'],
+        'jungle_trapdoor' => ['half' => 'HalfBlockPosition'],
+        'acacia_trapdoor' => ['half' => 'HalfBlockPosition'],
+        'dark_oak_trapdoor' => ['half' => 'HalfBlockPosition'],
+        'iron_trapdoor' => ['half' => 'HalfBlockPosition'],
+
+        'brick_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'cobblestone_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'stone_brick_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'nether_brick_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'sandstone_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'spruce_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'birch_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'jungle_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'quartz_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'acacia_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'dark_oak_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'oak_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'prismarine_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'prismarine_brick_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'dark_prismarine_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'red_sandstone_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+        'purpur_stairs' => ['half' => 'StairHalf', 'shape' => 'StairShape'],
+
+        'tall_seagrass' => ['half' => 'BlockHalf'],
+        'sunflower' => ['half' => 'BlockHalf'],
+        'lilac' => ['half' => 'BlockHalf'],
+        'rose_bush' => ['half' => 'BlockHalf'],
+        'peony' => ['half' => 'BlockHalf'],
+        'tall_grass' => ['half' => 'BlockHalf'],
+        'large_fern' => ['half' => 'BlockHalf'],
+
+        'spruce_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+        'birch_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+        'jungle_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+        'acacia_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+        'dark_oak_door' => ['half' => 'BlockHalf', 'hinge' => 'HingePosition'],
+
+        'comparator' => ['mode' => 'ComparatorMode'],
+        'redstone_wire' => ['east' => 'RedstonePosition', 'north' => 'RedstonePosition', 'south' => 'RedstonePosition', 'west' => 'RedstonePosition'],
+        'structure_block' => ['mode' => 'StructureBlockMode'],
+
+        'prismarine_slab' => ['type' => 'HalfBlockPosition'],
+        'prismarine_brick_slab' => ['type' => 'HalfBlockPosition'],
+        'dark_prismarine_slab' => ['type' => 'HalfBlockPosition'],
+        'oak_slab' => ['type' => 'HalfBlockPosition'],
+        'spruce_slab' => ['type' => 'HalfBlockPosition'],
+        'birch_slab' => ['type' => 'HalfBlockPosition'],
+        'jungle_slab' => ['type' => 'HalfBlockPosition'],
+        'acacia_slab' => ['type' => 'HalfBlockPosition'],
+        'dark_oak_slab' => ['type' => 'HalfBlockPosition'],
+        'stone_slab' => ['type' => 'HalfBlockPosition'],
+        'sandstone_slab' => ['type' => 'HalfBlockPosition'],
+        'petrified_oak_slab' => ['type' => 'HalfBlockPosition'],
+        'cobblestone_slab' => ['type' => 'HalfBlockPosition'],
+        'brick_slab' => ['type' => 'HalfBlockPosition'],
+        'stone_brick_slab' => ['type' => 'HalfBlockPosition'],
+        'nether_brick_slab' => ['type' => 'HalfBlockPosition'],
+        'quartz_slab' => ['type' => 'HalfBlockPosition'],
+        'red_sandstone_slab' => ['type' => 'HalfBlockPosition'],
+        'purpur_slab' => ['type' => 'HalfBlockPosition'],
+    ];
+
     /**
      * @var string
      */
@@ -15,6 +112,10 @@ class Property
      * @var array
      */
     private $states;
+    /**
+     * @var string
+     */
+    private $blockName;
 
     /**
      * Block constructor.
@@ -22,8 +123,9 @@ class Property
      * @param array $values
      * @param array $states
      */
-    public function __construct(string $propertyName, array $values, array $states)
+    public function __construct(string $blockName, string $propertyName, array $values, array $states)
     {
+        $this->blockName = $blockName;
         $this->propertyName = $propertyName;
         $this->values = $values;
         $this->states = $states;
@@ -65,7 +167,7 @@ class Property
             return 'PropertyBoolean';
         }
 
-        return 'PropertyEnum<' . ucfirst($this->propertyName) . '>';
+        return 'PropertyEnum<>';
     }
 
     private function getDefaultState(): array
@@ -90,6 +192,10 @@ class Property
 
         if ($this->isBoolean()) {
             return ', ' . $defaultValue;
+        }
+
+        if (isset(self::BLOCK_NAME_ENUM_OVERRIDE[$this->blockName][$this->propertyName])) {
+            return ', ' . ucfirst(self::BLOCK_NAME_ENUM_OVERRIDE[$this->blockName][$this->propertyName]) . '.' . strtoupper($defaultValue);
         }
 
         return ', ' . ucfirst($this->propertyName) . '.' . strtoupper($defaultValue);
--- src/main/java/rocks/cleanstone/game/block/state/property/vanilla/Instrument.java
+++ src/main/java/rocks/cleanstone/game/block/state/property/vanilla/Instrument.java
@@ -1,6 +1,5 @@
 package rocks.cleanstone.game.block.state.property.vanilla;
 
-public enum PistonType {
-    NORMAL,
-    STICKY
+public enum Instrument {
+    HARP;
 }
