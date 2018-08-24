60341f2237c77d0b3cf4058608e68ffa3c24610a
Tim Windelschmidt <fionera@fionera.de>
2018-08-24 20:17:40 +0200

    Add Cache workaround

--- src/main/java/rocks/cleanstone/game/block/state/SimpleBlockStateProvider.java
+++ src/main/java/rocks/cleanstone/game/block/state/SimpleBlockStateProvider.java
@@ -1,20 +1,21 @@
 package rocks.cleanstone.game.block.state;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
-
+import com.google.common.base.Objects;
+import org.springframework.cache.Cache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import rocks.cleanstone.game.block.state.property.Properties;
 import rocks.cleanstone.game.block.state.property.PropertiesBuilder;
 import rocks.cleanstone.game.block.state.property.Property;
 import rocks.cleanstone.game.material.block.BlockType;
 
-@CacheConfig(cacheNames = {"blockstates"})
 public class SimpleBlockStateProvider implements BlockStateProvider {
 
     private static BlockStateProvider INSTANCE;
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private final Cache cache;
+
+    public SimpleBlockStateProvider(CaffeineCacheManager caffeineCacheManager) {
+        cache = caffeineCacheManager.getCache("blockstates"); //TODO: Use annotation
+    }
 
     public static BlockStateProvider get() {
         return INSTANCE;
@@ -24,26 +25,22 @@ public class SimpleBlockStateProvider implements BlockStateProvider {
         INSTANCE = this;
     }
 
-    @Cacheable(cacheNames = "blockstates")
     @Override
     public BlockState of(BlockType blockType, Properties properties) {
-        logger.info("Calling of with two Params");
-        return new BlockState(blockType, properties);
+        return cache.get(Objects.hashCode(blockType, properties), () -> new BlockState(blockType, properties));
     }
 
-    @Cacheable(cacheNames = "blockstates")
     @Override
     public BlockState of(BlockType blockType) {
-        logger.info("Calling of with one Param " + blockType.hashCode());
-        return of(blockType, new Properties(blockType.getProperties()));
+        return cache.get(blockType.hashCode(), () -> of(blockType, new Properties(blockType.getProperties())));
     }
 
-    @Cacheable(cacheNames = "blockstates")
     @Override
     public <T> BlockState withProperty(BlockType blockType, Property<T> property, T value) {
-        logger.info("Calling of with three Params");
-        Properties properties = new PropertiesBuilder(blockType).withProperty(property, value).create();
-        return of(blockType, properties);
+        return cache.get(Objects.hashCode(blockType, property, value), () -> {
+            Properties properties = new PropertiesBuilder(blockType).withProperty(property, value).create();
+            return of(blockType, properties);
+        });
     }
 
     public void destroy() {
