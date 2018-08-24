3f5d75de2424842e57cdcaf36e1fea38fe142048
Leon Mangler <dev@myzelyam.de>
2018-08-24 14:58:41 +0200

    Add 1.13 ChunkData codec

---
+++ src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_13/outbound/ChunkDataCodec.java
@@ -0,0 +1,48 @@
+package rocks.cleanstone.net.minecraft.protocol.v1_13.outbound;
+
+import io.netty.buffer.ByteBuf;
+import rocks.cleanstone.game.block.state.mapping.BlockStateMapping;
+import rocks.cleanstone.game.world.chunk.data.block.vanilla.DirectPalette;
+import rocks.cleanstone.game.world.chunk.data.block.vanilla.VanillaBlockDataCodec;
+import rocks.cleanstone.game.world.chunk.data.block.vanilla.VanillaBlockDataStorage;
+import rocks.cleanstone.net.packet.Packet;
+import rocks.cleanstone.net.packet.outbound.ChunkDataPacket;
+import rocks.cleanstone.net.protocol.PacketCodec;
+import rocks.cleanstone.net.utils.ByteBufUtils;
+
+public class ChunkDataCodec implements PacketCodec {
+
+    private final BlockStateMapping<Integer> blockStateMapping;
+
+    public ChunkDataCodec(BlockStateMapping<Integer> blockStateMapping) {
+        this.blockStateMapping = blockStateMapping;
+    }
+
+    @Override
+    public Packet decode(ByteBuf byteBuf) {
+        throw new UnsupportedOperationException("ChunkData is outbound and cannot be decoded");
+    }
+
+    @Override
+    public ByteBuf encode(ByteBuf byteBuf, Packet packet) {
+        ChunkDataPacket chunkDataPacket = (ChunkDataPacket) packet;
+        byteBuf.writeInt(chunkDataPacket.getChunkX());
+        byteBuf.writeInt(chunkDataPacket.getChunkZ());
+        byteBuf.writeBoolean(chunkDataPacket.isGroundUpContinuous());
+
+        DirectPalette directPalette = new DirectPalette(blockStateMapping, 14);
+        VanillaBlockDataStorage storage = new VanillaBlockDataStorage(chunkDataPacket.getBlockDataTable(),
+                directPalette, true);
+
+        ByteBuf blockData = new VanillaBlockDataCodec(directPalette, false).serialize(storage);
+        byteBuf.writeBytes(blockData);
+        blockData.release();
+
+        ByteBufUtils.writeVarInt(byteBuf, 0);
+        //TODO encode NBT Tag array of block entities
+        //ByteBufUtils.writeVarInt(byteBuf,chunkDataPacket.getBlockEntities().length);
+        //byteBuf.writeBytes(chunkDataPacket.getBlockEntities())
+
+        return byteBuf;
+    }
+}
