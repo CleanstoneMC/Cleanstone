998a6c240a0274da173fc804f004934e06324eb2
Leon Mangler <dev@myzelyam.de>
2018-08-25 00:41:24 +0200

    Improve SimpleChunk coordinate validation

--- src/main/java/rocks/cleanstone/game/world/chunk/SimpleChunk.java
+++ src/main/java/rocks/cleanstone/game/world/chunk/SimpleChunk.java
@@ -48,17 +48,13 @@ public class SimpleChunk implements Chunk {
 
     @Override
     public Block getBlock(int x, int y, int z) {
-        Preconditions.checkArgument(
-                x >= 0 && x < Chunk.WIDTH && y >= 0 && y < Chunk.HEIGHT && z >= 0 && z < Chunk.WIDTH,
-                "Relative block coordinates out of bounds (" + x + ":" + y + ":" + z + ")");
+        validateRelativeBlockCoordinates(x, y, z);
         return blockDataTable.getBlock(x, y, z);
     }
 
     @Override
     public void setBlock(int x, int y, int z, Block block) {
-        Preconditions.checkArgument(
-                x >= 0 && x < Chunk.WIDTH && y >= 0 && y < Chunk.HEIGHT && z >= 0 && z < Chunk.WIDTH,
-                "Relative block coordinates out of bounds (" + x + ":" + y + ":" + z + ")");
+        validateRelativeBlockCoordinates(x, y, z);
         blockDataTable.setBlock(x, y, z, block);
         // TODO run the following expensive operation async
         blockDataStorage.setBlockState(x, y, z, block.getState());
@@ -67,11 +63,13 @@ public class SimpleChunk implements Chunk {
 
     @Override
     public byte getBlockLight(int x, int y, int z) {
+        validateRelativeBlockCoordinates(x, y, z);
         return blockDataTable.getBlockLight(x, y, z);
     }
 
     @Override
     public void setBlockLight(int x, int y, int z, byte blockLight) {
+        validateRelativeBlockCoordinates(x, y, z);
         blockDataTable.setBlockLight(x, y, z, blockLight);
         blockDataStorage.setBlockLight(x, y, z, blockLight);
         dirty = true;
@@ -79,11 +77,13 @@ public class SimpleChunk implements Chunk {
 
     @Override
     public byte getSkyLight(int x, int y, int z) {
+        validateRelativeBlockCoordinates(x, y, z);
         return blockDataTable.getSkyLight(x, y, z);
     }
 
     @Override
     public void setSkyLight(int x, int y, int z, byte skyLight) {
+        validateRelativeBlockCoordinates(x, y, z);
         if (!hasSkylight()) return;
         blockDataTable.setSkyLight(x, y, z, skyLight);
         blockDataStorage.setSkyLight(x, y, z, skyLight);
@@ -95,13 +95,13 @@ public class SimpleChunk implements Chunk {
         return blockDataTable.hasSkylight();
     }
 
-    public void setDirty(boolean dirty) {
-        this.dirty = dirty;
-    }
-
     @Override
     public boolean isDirty() {
         return dirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
     }
 
     @Override
@@ -112,5 +112,11 @@ public class SimpleChunk implements Chunk {
     @Override
     public BlockDataStorage getBlockDataStorage() {
         return blockDataStorage;
+    }
+
+    private void validateRelativeBlockCoordinates(int x, int y, int z) {
+        Preconditions.checkArgument(
+                x >= 0 && x < Chunk.WIDTH && y >= 0 && y < Chunk.HEIGHT && z >= 0 && z < Chunk.WIDTH,
+                "Relative block coordinates out of bounds (" + x + ":" + y + ":" + z + ")");
     }
 }
