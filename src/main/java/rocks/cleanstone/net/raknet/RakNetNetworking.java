a4c904ebd5fc399b8f2b7c30b1637fe0767a8710
Leon Mangler <dev@myzelyam.de>
2018-09-05 13:37:04 +0200

    Add SmartLifecycle to RakNetNetworking

--- src/main/java/rocks/cleanstone/net/raknet/RakNetNetworking.java
+++ src/main/java/rocks/cleanstone/net/raknet/RakNetNetworking.java
@@ -4,37 +4,60 @@ import com.whirvis.jraknet.identifier.Identifier;
 import com.whirvis.jraknet.server.RakNetServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.SmartLifecycle;
 import rocks.cleanstone.net.AbstractNetworking;
 import rocks.cleanstone.net.protocol.Protocol;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.Nonnull;
 import java.net.InetAddress;
 
-public class RakNetNetworking extends AbstractNetworking {
+public class RakNetNetworking extends AbstractNetworking implements SmartLifecycle {
 
-    private static int currentServerID = 0;
+    private static int currentServerID = 1;
 
     protected final Identifier identifier;
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private RakNetServer server;
+    private boolean running = false;
 
     public RakNetNetworking(int port, InetAddress address, Protocol protocol, Identifier identifier) {
         super(port, address, protocol);
         this.identifier = identifier;
     }
 
-    @PostConstruct
-    public void init() {
+    @Override
+    public void start() {
         server = new RakNetServer(port, Integer.MAX_VALUE, identifier);
         server.addListener(new ServerListener(this));
-        server.startThreaded().setName("RakNetServer-" + currentServerID++);
+        server.startThreaded().setName("rakNetServer-" + currentServerID++);
+        running = true;
     }
 
-    @EventListener
-    public void onPreDestroy(ContextClosedEvent e) {
+    @Override
+    public void stop() {
         logger.info("Closing " + protocol.getClass().getSimpleName());
         server.shutdown();
+        running = false;
+    }
+
+    @Override
+    public boolean isAutoStartup() {
+        return true;
+    }
+
+    @Override
+    public void stop(@Nonnull Runnable callback) {
+        stop();
+        callback.run();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public int getPhase() {
+        return 10;
     }
 }
