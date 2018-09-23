a785ed6a95f199f3340446e4c591dc13ffe2d277
Tim Windelschmidt <fionera@fionera.de>
2018-09-23 18:04:12 +0200

    Add Beans overview to Webinterface

---
+++ src/main/resources/web/spring-boot-admin-server-ui-extension/beans-endpoint.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="section" :class="{ 'is-loading' : !hasLoaded }">
+        <div v-if="error" class="message is-danger">
+            <div class="message-body">
+                <strong>
+                    <font-awesome-icon class="has-text-danger" icon="exclamation-triangle"/>
+                    Fetching beans failed.
+                </strong>
+                <p v-text="error.message"/>
+            </div>
+        </div>
+        <sba-panel :header-sticks-below="['#navigation']"
+                   v-for="(beanDefinition, beanName) in beans" :key="beanName"
+                   :title="beanName">
+            <table class="table is-fullwidth">
+                <tr v-for="(value, key) in beanDefinition" :key="`${key}-${value}`">
+                    <td>
+                        <span v-text="key"/><br>
+                        <small class="is-muted" v-if="value" v-text="value"/>
+                    </td>
+                </tr>
+            </table>
+            <p class="is-muted" v-else>No Beans set</p>
+        </sba-panel>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: {
+            instance: {
+                type: Object,
+                required: true
+            }
+        },
+        data: () => ({
+            hasLoaded: false,
+            error: null,
+            beans: null
+        }),
+        created() {
+            this.fetchBeans();
+        },
+        methods: {
+            async fetchBeans() {
+                this.error = null;
+                try {
+                    const res = await this.instance.axios.get('actuator/beans');
+                    this.beans = res.data.contexts.application.beans;
+                } catch (error) {
+                    console.warn('Fetching beans failed:', error);
+                    this.error = error;
+                }
+                this.hasLoaded = true;
+            }
+        }
+    }
+</script>
--- src/main/resources/web/spring-boot-admin-server-ui-extension/index.js
+++ src/main/resources/web/spring-boot-admin-server-ui-extension/index.js
@@ -1,4 +1,5 @@
 import consoleEndpoint from './console-endpoint';
+import beansEndpoint from './beans-endpoint';
 
 window.SBA.use({
     install({viewRegistry}) {
@@ -10,6 +11,17 @@ window.SBA.use({
             label: 'Console',
             order: 1000,
             isEnabled: ({instance}) => instance.hasEndpoint('console')
+        });
+
+        viewRegistry.addView({
+            name: 'instances/beans',
+            parent: 'instances',
+            path: 'beans',
+            component: beansEndpoint,
+            label: 'Beans',
+            group: 'Insights',
+            order: 1000,
+            isEnabled: ({instance}) => instance.hasEndpoint('beans')
         });
     }
 });
