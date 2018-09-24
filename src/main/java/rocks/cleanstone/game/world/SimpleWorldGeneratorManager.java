903743ff2770cfc60b89a2195c8d8cfd1ce7d34d
succcubbus <jannis.kaiser2@gmail.com>
2018-09-24 23:25:10 +0200

    use map in worldGeneratorManager

--- src/main/java/rocks/cleanstone/game/world/SimpleWorldGeneratorManager.java
+++ src/main/java/rocks/cleanstone/game/world/SimpleWorldGeneratorManager.java
@@ -1,23 +1,29 @@
 package rocks.cleanstone.game.world;
 
+import java.util.List;
+import java.util.Map;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rocks.cleanstone.game.world.generation.WorldGenerator;
 
-import java.util.List;
-
 @Component
 public class SimpleWorldGeneratorManager implements WorldGeneratorManager {
 
-    private List<? extends WorldGenerator> worldGenerators;
+    private Map<String, ? extends WorldGenerator> worldGenerators;
 
     @Autowired
     public SimpleWorldGeneratorManager(List<? extends WorldGenerator> worldGenerators) {
-        this.worldGenerators = worldGenerators;
+        this.worldGenerators = worldGenerators.stream()
+                .collect(toMap(
+                        WorldGenerator::getName,
+                        identity()
+                ));
     }
 
     @Override
     public WorldGenerator getWorldGenerator(String worldGeneratorName) {
-        return worldGenerators.stream().filter(worldGenerator -> worldGenerator.getName().equals(worldGeneratorName)).findFirst().orElse(null);
+        return worldGenerators.get(worldGeneratorName);
     }
 }
