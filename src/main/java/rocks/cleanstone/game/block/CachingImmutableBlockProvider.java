4b4a27619136c2b21447b5ef65a46d475ea70456
succcubbus <jannis.kaiser2@gmail.com>
2018-10-14 17:48:50 +0200

    replace spring caching with hashMaps

--- src/main/java/rocks/cleanstone/game/block/CachingImmutableBlockProvider.java
+++ src/main/java/rocks/cleanstone/game/block/CachingImmutableBlockProvider.java
@@ -5,26 +5,29 @@ import org.springframework.stereotype.Component;
 import rocks.cleanstone.game.block.state.BlockState;
 import rocks.cleanstone.game.block.state.property.Properties;
 import rocks.cleanstone.game.material.block.BlockType;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component("immutableBlockProvider")
 public class CachingImmutableBlockProvider {
+    private final Map<BlockState, ImmutableBlock> blockCache = new HashMap<>();
 
     public CachingImmutableBlockProvider() {
         ImmutableBlock.setLoadingSource(this);
     }
 
-    @Cacheable(value = "immutableBlocks", sync = true)
+    // @Cacheable(value = "immutableBlocks", sync = true)
     public ImmutableBlock of(BlockState state) {
-        return new ImmutableBlock(state);
+        return blockCache.computeIfAbsent(state, ImmutableBlock::new);
     }
 
-    @Cacheable(value = "immutableBlocks", sync = true)
+    // @Cacheable(value = "immutableBlocks", sync = true)
     public ImmutableBlock of(BlockType blockType) {
         BlockState blockState = BlockState.of(blockType);
         return ImmutableBlock.of(blockState);
     }
 
-    @Cacheable(value = "immutableBlocks", sync = true)
+    // @Cacheable(value = "immutableBlocks", sync = true)
     public ImmutableBlock of(BlockType blockType, Properties properties) {
         BlockState blockState = BlockState.of(blockType, properties);
 
--- src/main/java/rocks/cleanstone/game/block/state/CachingBlockStateProvider.java
+++ src/main/java/rocks/cleanstone/game/block/state/CachingBlockStateProvider.java
@@ -6,25 +6,31 @@ import rocks.cleanstone.game.block.state.property.Properties;
 import rocks.cleanstone.game.block.state.property.PropertiesBuilder;
 import rocks.cleanstone.game.block.state.property.Property;
 import rocks.cleanstone.game.material.block.BlockType;
+import java.util.Map;
+import java.util.HashMap;
 
 @Component("blockStateProvider")
 public class CachingBlockStateProvider {
+    private final Map<BlockType, Map<Properties, BlockState>> blockStateCache = new HashMap<>();
 
     public CachingBlockStateProvider() {
         BlockState.setLoadingSource(this);
     }
 
-    @Cacheable(value = "blockStates", sync = true)
+    // @Cacheable(value = "blockStates", sync = true)
     public BlockState of(BlockType blockType, Properties properties) {
-        return new BlockState(blockType, properties);
+        return blockStateCache.computeIfAbsent(
+                blockType,
+                b -> new HashMap<>()
+        ).computeIfAbsent(properties, p -> new BlockState(blockType, properties));
     }
 
-    @Cacheable(value = "blockStates", sync = true)
+    // @Cacheable(value = "blockStates", sync = true)
     public BlockState of(BlockType blockType) {
         return BlockState.of(blockType, new Properties(blockType.getProperties()));
     }
 
-    @Cacheable(value = "blockStates", sync = true)
+    // @Cacheable(value = "blockStates", sync = true)
     public <T> BlockState withProperty(BlockType blockType, Property<T> property, T value) {
         Properties properties = new PropertiesBuilder(blockType).withProperty(property, value).create();
 
