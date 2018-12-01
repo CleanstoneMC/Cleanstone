74f723c4ffbf79963699ff5ff7506db27f23cd57
Tim Windelschmidt <fionera@fionera.de>
2018-12-01 01:00:17 +0100

    Fix Slot decoding in 1.13.1 and 1.13.2

--- src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_13_1/inbound/CreativeInventoryActionCodec.java
+++ src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_13_1/inbound/CreativeInventoryActionCodec.java
@@ -1,20 +1,20 @@
 package rocks.cleanstone.net.minecraft.protocol.v1_13_1.inbound;
 
 import com.google.common.base.Preconditions;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Nullable;
-
 import io.netty.buffer.ByteBuf;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import rocks.cleanstone.game.inventory.item.ItemStack;
 import rocks.cleanstone.game.inventory.item.SimpleItemStack;
 import rocks.cleanstone.game.material.item.ItemType;
 import rocks.cleanstone.game.material.item.mapping.ItemTypeMapping;
 import rocks.cleanstone.net.minecraft.packet.inbound.CreativeInventoryActionPacket;
 import rocks.cleanstone.net.protocol.InboundPacketCodec;
+import rocks.cleanstone.net.utils.ByteBufUtils;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
 
 @Slf4j
 @Component
@@ -24,11 +24,10 @@ public class CreativeInventoryActionCodec implements InboundPacketCodec<Creative
 
     public CreativeInventoryActionCodec(@Qualifier("protocolItemTypeMapping_v1_13_1") ItemTypeMapping<Integer> itemTypeMapping) {
         this.itemTypeMapping = itemTypeMapping;
-
     }
 
     @Override
-    public CreativeInventoryActionPacket decode(ByteBuf byteBuf) {
+    public CreativeInventoryActionPacket decode(ByteBuf byteBuf) throws IOException {
         short slot = byteBuf.readShort();
         ItemStack clickedItem = readItemStack(byteBuf);
 
@@ -36,13 +35,15 @@ public class CreativeInventoryActionCodec implements InboundPacketCodec<Creative
     }
 
     @Nullable
-    private ItemStack readItemStack(ByteBuf byteBuf) {
-        short itemID = byteBuf.readShort();
-        log.info("Reading itemID " + itemID + " with " + itemTypeMapping.getClass().getSimpleName());
+    private ItemStack readItemStack(ByteBuf byteBuf) throws IOException {
+        boolean present = byteBuf.readBoolean();
 
-        if (itemID != -1) {
+        if (present) {
+            int itemID = ByteBufUtils.readVarInt(byteBuf);
+            log.info("Reading itemID " + itemID + " with " + itemTypeMapping.getClass().getSimpleName());
+
             byte itemCount = byteBuf.readByte();
-            ItemType itemType = itemTypeMapping.getItemType((int) itemID);
+            ItemType itemType = itemTypeMapping.getItemType(itemID);
             Preconditions.checkNotNull(itemType, "Cannot find itemType with ID " + itemID);
             byte nbtStartByte = byteBuf.readByte(); // TODO Item NBT
             return new SimpleItemStack(itemType, itemCount, null);
