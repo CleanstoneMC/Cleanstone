4da6a0cd91d7b0b2a4620db4bb21d6f796eb2298
Leon Mangler <dev@myzelyam.de>
2019-02-06 15:02:34 +0100

    Implement SimpleEntityConverterRegistry

--- src/main/java/rocks/cleanstone/net/minecraft/entity/metadata/SimpleEntityConverterRegistry.java
+++ src/main/java/rocks/cleanstone/net/minecraft/entity/metadata/SimpleEntityConverterRegistry.java
@@ -2,23 +2,29 @@ package rocks.cleanstone.net.minecraft.entity.metadata;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import rocks.cleanstone.game.entity.Entity;
 
 @Component
 public class SimpleEntityConverterRegistry implements EntityConverterRegistry {
 
+    private final Map<Class<?>, EntityConverter<?>> entityClassConverterMap = new ConcurrentHashMap<>();
+
     @Override
     public <T extends Entity> void registerConverter(Class<T> entityClass, EntityConverter<T> converter) {
-        // TODO
+        entityClassConverterMap.put(entityClass, converter);
     }
 
     @Override
     public void unregisterConverter(Class<? extends Entity> entityClass) {
-        // TODO
+        entityClassConverterMap.remove(entityClass);
     }
 
     @Override
     public <T extends Entity> EntityConverter<T> getConverter(Class<T> entityClass) {
-        return null;
+        //noinspection unchecked
+        return (EntityConverter<T>) entityClassConverterMap.get(entityClass);
     }
 }
