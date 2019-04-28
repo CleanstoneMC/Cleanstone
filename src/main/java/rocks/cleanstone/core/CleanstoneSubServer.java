f0a995eb403bb4e0f2b3a1791209792d6120a8e8
Leon Mangler <dev@myzelyam.de>
2019-04-28 02:01:15 +0200

    Fix compilation issues with CleanstoneSubServer

--- src/main/java/rocks/cleanstone/core/CleanstoneSubServer.java
+++ src/main/java/rocks/cleanstone/core/CleanstoneSubServer.java
@@ -1,10 +1,10 @@
 package rocks.cleanstone.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
 import rocks.cleanstone.core.config.CleanstoneConfig;
 import rocks.cleanstone.core.config.MinecraftConfig;
 
@@ -17,15 +17,8 @@ import rocks.cleanstone.core.config.MinecraftConfig;
 @Profile(value = "subServer")
 public class CleanstoneSubServer extends CleanstoneServer {
 
-    private ExternalServer mainServer;
-
     @Autowired
     public CleanstoneSubServer(CleanstoneConfig cleanstoneConfig, MinecraftConfig minecraftConfig) {
         super(cleanstoneConfig, minecraftConfig);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
     }
 }
