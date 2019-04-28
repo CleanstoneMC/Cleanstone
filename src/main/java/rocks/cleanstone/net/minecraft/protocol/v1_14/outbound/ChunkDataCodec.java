fde9718cd52ac43127297e02d950d6c0490a1525
Fionera <fionera@fionera.de>
2019-04-28 02:17:38 +0200

    fixes but nbt still broken

--- src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_14/outbound/ChunkDataCodec.java
+++ src/main/java/rocks/cleanstone/net/minecraft/protocol/v1_14/outbound/ChunkDataCodec.java
@@ -49,26 +49,30 @@ public class ChunkDataCodec implements OutboundPacketCodec<ChunkDataPacket> {
 
         ByteBuf blockData = vanillaBlockDataCodecFactory.get(directPalette, true).encode(storage);
 
-
         ByteBuf newData = Unpooled.buffer();
 
         val primaryBitMask = ByteBufUtils.readVarInt(blockData);
         ByteBufUtils.writeVarInt(newData, primaryBitMask);
 
+        int[] nonAirBlockCounts = new int[16];
+        val table = storage.constructTable();
         int[] motionBlocking = new int[16 * 16];
         for (int sections = 0; sections < 16; sections++) {
+            int nonAirBlockCount = 0;
             for (int x = 0; x < 16; x++) {
                 for (int y = 0; y < 16; y++) {
                     for (int z = 0; z < 16; z++) {
-                        final BlockState blockState = storage.constructTable().getBlock(x, y, z).getState();
+                        final BlockState blockState = table.getBlock(x, y, z).getState();
                         if (blockState.getBlockType() != VanillaBlockType.AIR && blockState.getBlockType() != VanillaBlockType.CAVE_AIR && blockState.getBlockType() != VanillaBlockType.VOID_AIR) {
-//                            nonAirBlockCount++;
+                            nonAirBlockCount++;
                         }
 
                         motionBlocking[x + z * 16] = y + sections * 16 + 2; // Should be +1 (top of the block) but +2 works :tm:
                     }
                 }
             }
+
+            nonAirBlockCounts[sections] = nonAirBlockCount;
         }
 
         // Encode NBT Heightmaps
@@ -78,9 +82,12 @@ public class ChunkDataCodec implements OutboundPacketCodec<ChunkDataPacket> {
             val compoundTag = new CompoundTag();
             compoundTag.put("MOTION_BLOCKING", longArrayTag);
 
+            val encodingTag = new CompoundTag();
+            encodingTag.put("", compoundTag);
+
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             try {
-                compoundTag.serializeValue(new DataOutputStream(byteArrayOutputStream), Tag.DEFAULT_MAX_DEPTH);
+                encodingTag.serializeValue(new DataOutputStream(byteArrayOutputStream), Tag.DEFAULT_MAX_DEPTH);
             } catch (IOException i) {
 
             }
@@ -91,34 +98,43 @@ public class ChunkDataCodec implements OutboundPacketCodec<ChunkDataPacket> {
         val dataSize = ByteBufUtils.readVarInt(blockData);
         ByteBufUtils.writeVarInt(newData, dataSize);
 
-        for (int i = 0; i < dataSize; i++) {
+        // Read Chunk Sections
+        for (int i = 0; i < 16; i++) {
+            newData.writeShort(nonAirBlockCounts[i]);
+
             val bitsPerBlock = blockData.readByte();
             newData.writeByte(bitsPerBlock);
 
-            val paletteLength = ByteBufUtils.readVarInt(blockData);
-            ByteBufUtils.writeVarInt(newData, paletteLength);
+            if (bitsPerBlock >= 9) {
+                val paletteLength = ByteBufUtils.readVarInt(blockData);
+                ByteBufUtils.writeVarInt(newData, paletteLength);
 
-            for (int j = 0; j < paletteLength; j++) {
-                newData.writeLong(blockData.readLong());
+                // Read Palette
+                for (int j = 0; j < paletteLength; j++) {
+                    newData.writeLong(blockData.readLong());
+                }
             }
 
             val dataLength = ByteBufUtils.readVarInt(blockData);
             ByteBufUtils.writeVarInt(newData, dataLength);
 
+            // Read DataArray
             for (int j = 0; j < dataLength; j++) {
                 newData.writeLong(blockData.readLong());
             }
 
-            for (int j = 0; j < 16 * 16 * 256 / 2; j++) {
+            // Read BlockLight
+            for (int j = 0; j < 16; j++) {
                 blockData.readByte();
             }
 
-            for (int j = 0; j < 16 * 16 * 256 / 2; j++) {
-                blockData.readByte();
+            if (storage.getSection(i).hasSkyLight()) {
+                // Read SkyLight
+                for (int j = 0; j < 16; j++) {
+                    blockData.readByte();
+                }
             }
         }
-
-        System.out.println(blockData.readableBytes());
 
         newData.writeInt(127);
 
