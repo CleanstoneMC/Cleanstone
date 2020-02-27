8be2080e61283d382e6877d799d5fdb01d191e93
Fionera <fionera@fionera.de>
2020-02-27 13:53:04 +0100

    Add package-info for logic package

---
+++ src/main/java/rocks/cleanstone/logic/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * All listeners and other handlers regarding Gamelogic
+ * <p>
+ * This contains as example the handling of the abstract game events like a {@link rocks.cleanstone.player.event.PlayerMoveEvent}
+ * which gets converted and thrown by the listeners in {@link rocks.cleanstone.endpoint.minecraft.java.net.listener.inbound.PlayerMovePacketListener}
+ * </p>
+ */
+package rocks.cleanstone.logic;
