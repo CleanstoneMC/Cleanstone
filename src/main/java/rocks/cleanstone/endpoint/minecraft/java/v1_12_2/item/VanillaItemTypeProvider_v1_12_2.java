ca4322213f650dabcb6419a1bd501c1a2681fde5
Fionera <fionera@fionera.de>
2020-02-27 16:24:59 +0100

    Generate ItemTypeProviders

---
+++ src/main/java/rocks/cleanstone/endpoint/minecraft/java/v1_12_2/item/VanillaItemTypeProvider_v1_12_2.java
@@ -0,0 +1,23 @@
+package rocks.cleanstone.endpoint.minecraft.java.v1_12_2.item;
+
+import org.springframework.stereotype.Component;
+import rocks.cleanstone.game.material.MaterialRegistry;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
+@Component()
+public class VanillaItemTypeProvider_v1_12_2 {
+
+    private MaterialRegistry materialRegistry;
+
+    public VanillaItemTypeProvider_v1_12_2(MaterialRegistry materialRegistry) {
+        this.materialRegistry = materialRegistry;
+    }
+
+    @PostConstruct
+    public void registerVanillaItemTypes() {
+        Arrays.stream(VanillaItemType_v1_12_2.values()).forEach(materialRegistry::registerItemType);
+    }
+
+}
