d9ccde2615edfe67981c72f5b287abec67df0fb8
Leon Mangler <dev@myzelyam.de>
2020-03-01 22:36:36 +0100

    Add 1.14.1, 1.14.2, 1.14.3, 1.14.4 protocol layers

--- src/main/java/rocks/cleanstone/endpoint/minecraft/vanilla/net/protocol/MinecraftClientProtocolLayer.java
+++ src/main/java/rocks/cleanstone/endpoint/minecraft/vanilla/net/protocol/MinecraftClientProtocolLayer.java
@@ -1,17 +1,22 @@
 package rocks.cleanstone.endpoint.minecraft.vanilla.net.protocol;
 
-import rocks.cleanstone.net.protocol.ClientProtocolLayer;
-
-import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.Comparator;
+
+import javax.annotation.Nullable;
+
+import rocks.cleanstone.net.protocol.ClientProtocolLayer;
 
 public enum MinecraftClientProtocolLayer implements ClientProtocolLayer {
     MINECRAFT_V1_12_2("1.12.2", 340),
     MINECRAFT_V1_13("1.13", 393),
     MINECRAFT_V1_13_1("1.13.1", 401),
     MINECRAFT_V1_13_2("1.13.2", 404),
-    MINECRAFT_V1_14("1.14", 477);
+    MINECRAFT_V1_14("1.14", 477),
+    MINECRAFT_V1_14_1("1.14.1", 480),
+    MINECRAFT_V1_14_2("1.14.2", 485),
+    MINECRAFT_V1_14_3("1.14.3", 490),
+    MINECRAFT_V1_14_4("1.14.4", 498);
 
     private final String name;
     private final int versionNumber;
